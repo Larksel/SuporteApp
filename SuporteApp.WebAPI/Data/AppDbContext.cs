009d0312acee2bb08db69948195d4df6b43df9c0
Larksel <lemuelgames@outlook.com>
2025-11-25 18:43:12 -0300

    :sparkles: feat: Adicionado dados iniciais

--- SuporteApp.WebAPI/Data/AppDbContext.cs
+++ SuporteApp.WebAPI/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using SuporteApp.WebAPI.Objects.Enums;
 using SuporteApp.WebAPI.Objects.Models;
 
 namespace SuporteApp.WebAPI.Data;
@@ -24,5 +25,50 @@ public class AppDbContext : DbContext
             .WithMany(u => u.TicketsAssigned)
             .HasForeignKey(t => t.TechnicianId)
             .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Name = "Cliente João",
+                    Email = "joao@cliente.com",
+                    Password = "123",
+                    Role = UserRole.Client
+                },
+                new User
+                {
+                    Id = 2,
+                    Name = "Técnica Maria",
+                    Email = "maria@suporte.com",
+                    Password = "123",
+                    Role = UserRole.Technician
+                }
+            );
+
+        // Ticket Inicial
+        modelBuilder.Entity<Ticket>().HasData(
+            new Ticket
+            {
+                Id = 1,
+                Title = "Internet lenta e caindo",
+                Description = "Minha conexão cai toda vez que abro o Teams.",
+                CreatedAt = DateTime.UtcNow,
+                Status = TicketStatus.Open,
+                ClientId = 1,
+                TechnicianId = null
+            }
+        );
+
+        // Já deixar uma mensagem inicial no ticket
+        modelBuilder.Entity<Message>().HasData(
+            new Message
+            {
+                Id = 1,
+                Content = "Olá, estou com esse problema urgente.",
+                SentAt = DateTime.UtcNow,
+                TicketId = 1,
+                SenderId = 1
+            }
+        );
     }
 }
